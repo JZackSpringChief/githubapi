eb0c1ee777a09b24ac4944fcea66500e9a6c0f6a
Zack Spring Chief <jzacksc@gmail.com>
2023-08-18 21:54:05 -0600

    added await request

--- octokit.js
+++ octokit.js
@@ -1,5 +1,13 @@
 import { Octokit } from "octokit";
+import { config } from "dotenv";
+
+config();
 
 export const octokit = new Octokit({
-  auth: "YOUR-TOKEN",
+  auth: process.env.GIT_KEY,
+});
+
+await octokit.request("GET /repos/{owner}/{repo}/issues", {
+  owner: "octocat",
+  repo: "Spoon-Knife",
 });
