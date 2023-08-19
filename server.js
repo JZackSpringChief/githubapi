8f36417f31ada549b09536d98ff7ddc745e7ded8
Zack Spring Chief <jzacksc@gmail.com>
2023-08-18 21:22:14 -0600

    adding octokit

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 import express from "express";
+import { Octokit } from "octokit";
+
 const app = express();
 const port = 5524;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("server is up");
@@ -8,4 +13,8 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log(`server is up on port ${port}`);
+});
+
+const octokit = new Octokit({
+  auth: "YOUR-TOKEN",
 });
