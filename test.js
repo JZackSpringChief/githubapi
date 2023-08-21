470546e1d66a4722648ee1229f3defdd3b127d4a
Zack Spring Chief <jzacksc@gmail.com>
2023-08-21 14:28:51 -0600

    adding some test and study functions to test

--- test.js
+++ test.js
@@ -4,3 +4,28 @@ let go = true;
 let stop = false;
 
 console.log(go !== stop);
+
+//logical operators "or" ||
+
+let condition1 = true;
+let condition2 = false;
+
+const logicalExpression = condition1 || condition2;
+console.log(!logicalExpression || logicalExpression);
+
+let isLoanApproved = true;
+let cash = 30000;
+let cost = 40000;
+const isCarBought = isLoanApproved || cash > cost;
+console.log(isCarBought);
+
+let isLoadRejected = false;
+const isLoanGranted = !isLoadRejected;
+console.log(isLoanGranted);
+
+let cloudStorage = 4;
+let systemStorage = 8;
+let fileSize = 5;
+
+const result = cloudStorage > fileSize || systemStorage > fileSize;
+console.log("Saved? : " + result);
